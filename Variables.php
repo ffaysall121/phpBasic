ff33cf3ce11005dc09989ef3c2a9fe6d7ff84193
MdFaysalAhmed <ffaysall121@gmail.com>
2022-08-09 00:12:42 +0600

    Variables and Print

---
+++ Variables.php
@@ -0,0 +1,10 @@
+<?php
+// use "$" symbol before the variables name to declaring all kinds of variables in php.
+# $variablesNames = values.
+$name="Faysal Ahmed"; //string
+$id=35024; //integer
+$section='A';//character 
+$cgpa=2.95; //float
+
+echo "Name =" ,$name  ," ID =" ,$id ," Section =", $section," CGPA =", $cgpa ;
+?>
