c9e85c787959aa9ff90a43e66806735a34472947
MdFaysalAhmed <ffaysall121@gmail.com>
2022-08-08 22:44:26 +0600

    first commit

--- syntax.php
+++ syntax.php
@@ -1,3 +1,18 @@
 <?php //<-start with it
 echo'hello world'; //code
 //end with it->  ?>
+
+
+<?php
+        // This is a single-line comment
+        
+        # This is also a single-line comment
+ ?> 
+
+<?php
+/*
+This is a multiple-lines comment block
+that spans over multiple
+lines
+*/
+?>
